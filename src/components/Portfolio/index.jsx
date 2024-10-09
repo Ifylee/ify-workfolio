b8025265bf5b31026304f715290d3405b95c85d3
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-10-09 17:17:24 -0500

    restructured and added codes

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -6,34 +6,40 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [activities] = useState([
     {
-      name: 'surfReport',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'blog-vista',
+      description: 'CMS-style-blog',
+      link: "https://blog-vista-dmdu.onrender.com",
+      repo: "https://github.com/Ifylee/blog_vista"
     },
     {
-      name: 'pastelPuzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'weather-channel',
+      description: 'Server-side-APIs',  
+      link: "https://ifylee.github.io/weather-channel/",
+      repo: "https://github.com/Ifylee/weather-channel"
     },
     {
-      name: 'runBuddy',
+      name: 'list-it',
+      description: 'MERN-stack',
+      link: "https://list-it-kw5e.onrender.com/",
+      repo: "https://github.com/Ifylee/List-It"
+    },
+    {
+      name: 'dream-voyage',
+      description: 'MERN-stack',
+      link: "https://dream-voyage.onrender.com/",
+      repo: "https://github.com/Ifylee/Dream-Voyage"
+    },
+    {
+      name: 'code-refactor',
       description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://ifylee.github.io/business-marketing-strategies/",
+      repo: "https://github.com/Ifylee/business-marketing-strategies"
     },
-    {
-      name: 'ledWall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+     {
+      name: 'graphbooks',
+      description: 'MERN-stack',
+      link: "https://graphbooks.onrender.com",
+      repo: "https://github.com/Ifylee/graphBooks"
     },
   ]);
 
