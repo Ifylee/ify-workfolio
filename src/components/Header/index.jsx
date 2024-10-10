1dde2691b599f60ac145901eeb20710ad686f905
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-10-10 18:14:21 -0500

    debug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ function Header(props) {
     return (
         <header className="flex-row space-between px-1">
         <h1>Ify-Workfolio</h1>
-        <img src={coverPix} alt="image of an open laptop on a table"></img>
+        <img src={coverPix} alt="picture of a serene landscape of flowers and trees with a resting bench by the side of the walkway"></img>
         {props.children}
         </header>
     );
