2669b4ba1b786c5b5a8d3da5ffaa829384331907
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-10-09 19:07:21 -0500

    added images

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ function Header(props) {
     return (
         <header className="header-container">
         <h1>Ify-Workfolio</h1>
-        <img src="https://plus.unsplash.com/premium_photo-1667587245819-2bea7a93e7a1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBvcnRmb2xpbyUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D" alt="background"/>
+        <img src="C:\Users\ifyek\OneDrive\Desktop\ify-workfolio\src\asset\images\coverImage.png"/>
         {props.children}
         </header>
     );
