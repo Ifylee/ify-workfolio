301e20eae7f8cb74a19fad7aaa0e034bc4ec4c54
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-10-11 07:13:12 -0500

    more comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// Importing necessary libraries and components
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
@@ -8,32 +9,35 @@ import Contact from './components/Contact';
 import Resume from './components/Resume';
 import './index.css';
 
+// Creating a browser router with defined routes
 const router = createBrowserRouter([
   {
+    // Defining the base route
     path: '/',
-    element: <App />,
-    errorElement: <Error />,
+    element: <App />, // Rendering the main App component
+    errorElement: <Error />, // Rendering the error page component
     children: [
       {
-        index: true,
-        element: <About />,
+        index: true, // Default route when path is '/'
+        element: <About />,  // Component to render for the default route
       },
       {
-        path: 'portfolio',
-        element: <Portfolio />,
+        path: 'portfolio', // Route for the Portfolio page
+        element: <Portfolio />, // Component to render for the Portfolio page
       },
       {
-        path: 'contact',
-        element: <Contact />,
+        path: 'contact', // Route for the Contact page
+        element: <Contact />, // Component to render for the Contact page
       },
       {
-        path: 'resume',
-        element: <Resume />,
+        path: 'resume', // Route for the Resume page
+        element: <Resume />, // Component to render for the Resume page
       },
     ],
   },
 ]);
 
+// Rendering the RouterProvider component with the router
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
