51c89dd5b5e4ae632164c6578dee884feb044d44
Ifylee <159982321+Ifylee@users.noreply.github.com>
2025-01-17 18:21:12 -0600

    edited

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -15,16 +15,15 @@ function About() {
           <p>
           I caught fire coding.</p>
           <p>
-          Who could have imagined I'd become a coder? Born and raised in Nigeria, I earned my undergraduate degree in French and began a rewarding career as a Sales Manager for an underwater engineering company. This role allowed me to travel to countries like Taiwan, Switzerland, France, and Germany, blending professional development with adventure. </p>
+          Who could have imagined I’d become a coder? Born and raised in Nigeria, I earned my degree in French and began a rewarding career as a Sales Manager for an underwater engineering company. This role took me to Taiwan, Switzerland, France, and Germany, blending professional growth with adventure. </p>
           <p>
-          In 2017, I visited America, where unexpected twists reshaped my life. I fell in love, got married, and started a new chapter as a wife. My career took off as a customer service representative, and within three months, I was promoted to Shipping Supervisor.</p>
+          In 2017, a trip to America transformed my life. I fell in love, got married, and began a new chapter as a wife. My career flourished as I transitioned to customer service, quickly earning a promotion to Shipping Supervisor within three months.</p>
          <p>
-          During maternity leave, an ad for Codecademy caught my attention, sparking my journey into coding. I’ll never forget the moment I wrote my first line of code—"Hello, world." It felt magical as if I were the magician creating something extraordinary. That moment sparked an inextinguishable fire in me, awakening a passion for creating and problem-solving. All I wanted to do was code, code, code, code!</p>
+         During maternity leave, I stumbled upon an ad for Codecademy, igniting my journey into coding. I’ll never forget writing my first line of code—"Hello, world." It felt magical, as if I were conjuring something extraordinary. That moment sparked an unquenchable passion for creating and problem-solving. All I wanted to do was code!</p>
           <p>
-          Motivated by my newfound passion, I enrolled in Codecademy’s full program and completed HTML, CSS, and JavaScript courses. I discovered that coding is like solving puzzles—each piece fitting into a grand vision. The satisfaction of building apps gave me a sense of empowerment: "If I want it, I can build it. Period!"</p>
+          Motivated by this passion, I enrolled in Codecademy’s full program, completing courses in HTML, CSS, and JavaScript. I discovered coding to be like solving puzzles, where each piece fits into a grand vision. The satisfaction of building apps empowered me: "If I want it, I can build it. Period!"</p>
           <p>
-          To solidify my knowledge, I took a career break and enrolled in UC Berkeley Extension’s 24-week intensive program in web development. It was a transformative journey that equipped me with front-end and back-end development skills and a deep understanding of full-stack technologies. Transitioning from a background in sales and logistics to information technology has been challenging, but it’s also what makes me unique. I bring a wealth of experience in customer service, communication, sales, and time management—skills that complement my technical expertise. 
-          I am excited to combine these strengths to create meaningful solutions and add value wherever I go.
+          To deepen my knowledge, I took a career break and enrolled in UC Berkeley Extension’s 24-week intensive web development program. This transformative experience equipped me with front-end and back-end skills and a deep understanding of full-stack technologies. Transitioning from sales and logistics to information technology has been challenging, yet it makes me unique. I bring a wealth of experience in customer service, communication, sales, and time management—skills that complement my technical expertise. I am excited to combine these strengths to create meaningful solutions and add value wherever I go.
           </p>
         </div>
     </section>
