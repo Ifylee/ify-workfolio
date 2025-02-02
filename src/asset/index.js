47011427ee1463ac1912bf6bc7f59bc3b053e850
Ifylee <159982321+Ifylee@users.noreply.github.com>
2025-02-02 16:41:44 -0600

    updated

--- src/asset/index.js
+++ src/asset/index.js
@@ -1,6 +1,6 @@
 export { default as blogVista } from './activities/blogVista.png';
-export { default as codeRefactor } from './activities/codeRefactor.png';
-export { default as dreamVoyage } from './activities/dreamVoyage.png';
+export { default as digitalnotebook } from './activities/digitalnotebook.png';
+export { default as scheduleboard } from './activities/scheduleboard.png';
 export { default as graphbooks} from './activities/graphbooks.png';
 export { default as listIt } from './activities/listIt.png';
 export { default as weatherChannel } from './activities/weatherChannel.png';
