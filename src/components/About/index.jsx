7019e76a14ce32c8c8c48d308234f54d74b4b8b2
Ifylee <159982321+Ifylee@users.noreply.github.com>
2025-02-02 15:50:15 -0600

    updated

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -15,15 +15,15 @@ function About() {
           <p>
           I caught fire coding.</p>
           <p>
-          Who could have imagined I’d become a coder? Born and raised in Nigeria, I earned my degree in French and began a rewarding career as a Sales Manager for an underwater engineering company. This role took me to Taiwan, Switzerland, France, and Germany, blending professional growth with adventure. </p>
+          Who could have imagined I’d become a coder? Born and raised in Nigeria, I earned my degree in French and started a rewarding career as a Customer service representative. I quickly rose to the position of Marketing Manager for an underwater engineering company, a role that took me to Taiwan, Switzerland, France, and Germany, blending professional growth with adventure. </p>
           <p>
-          In 2017, a trip to America transformed my life. I fell in love, got married, and began a new chapter as a wife. My career flourished as I transitioned to customer service, quickly earning a promotion to Shipping Supervisor within three months.</p>
+          In 2020, I took a trip to America that transformed my life in ways I never imagined. I fell in love, got married, and began a new chapter as a wife in the US. Little did I know, God would bless me so quickly with the gift of an adorable baby boy </p>
          <p>
-         During maternity leave, I stumbled upon an ad for Codecademy, igniting my journey into coding. I’ll never forget writing my first line of code—"Hello, world." It felt magical, as if I were conjuring something extraordinary. That moment sparked an unquenchable passion for creating and problem-solving. All I wanted to do was code!</p>
+         During my maternity leave, I stumbled upon an ad for Codecademy, sparking the beginning of my coding journey. I’ll never forget writing my first line of code—"Hello, world." It felt magical, as if I were conjuring something extraordinary. That moment ignited an unquenchable passion for creating and problem-solving. All I wanted to do was code!</p>
           <p>
-          Motivated by this passion, I enrolled in Codecademy’s full program, completing courses in HTML, CSS, and JavaScript. I discovered coding to be like solving puzzles, where each piece fits into a grand vision. The satisfaction of building apps empowered me: "If I want it, I can build it. Period!"</p>
+          Driven by this passion, I enrolled in Codecademy’s full program, completing courses in HTML, CSS, and JavaScript. I quickly realized coding is like solving puzzles, where each piece fits into a larger vision. The satisfaction of building apps empowered me: "If I want it, I can build it. Period!"</p>
           <p>
-          To deepen my knowledge, I enrolled in UC Berkeley Extension’s 24-week intensive web development program. This transformative experience equipped me with front-end and back-end skills and a deep understanding of full-stack technologies. Transitioning from sales and logistics to information technology has been challenging, yet it makes me unique. I bring a wealth of experience in customer service, communication, sales, and time management—skills that complement my technical expertise. I am excited to combine these strengths to create meaningful solutions and add value wherever I go.
+          To deepen my knowledge, I enrolled in UC Berkeley Extension’s 24-week intensive web development program. This transformative experience equipped me with front-end and back-end skills, as well as a deep understanding of full-stack technologies. Transitioning from marketing to information technology has been challenging, but it makes me unique. I bring a wealth of experience in customer service, communication, sales, and time management—skills that complement my technical expertise. I am excited to combine these strengths to create meaningful solutions and add value wherever I go.
           </p>
         </div>
     </section>
